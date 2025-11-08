d0f494d9bb6c0ab5654b69f0ef28517b64c3a998
techted89 <45821909+techted89@users.noreply.github.com>
2025-11-07 16:09:40 -0800

    Update applicationId in build.gradle.kts

--- App/build.gradle.kts
+++ App/build.gradle.kts
@@ -8,7 +8,7 @@ android {
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.geminichat"
+        applicationId = "com.techtedapps.geminichat"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
