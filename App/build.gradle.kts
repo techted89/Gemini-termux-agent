80a3528f893755ebb90d149c5d98426d53a38893
techted89 <45821909+techted89@users.noreply.github.com>
2025-11-08 00:38:54 -0800

    Update build.gradle.kts

--- App/build.gradle.kts
+++ App/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("com.android.application") version "8.1.1"
-    id("org.jetbrains.kotlin.android") version "1.8.10"
+    id("com.android.application") version "8.13.1"
+    id("org.jetbrains.kotlin.android") version "2.2.21"
 }
 
 android {
