5a281d8fa4af303c1523d2f804d62d97e816d77d
techted89 <45821909+techted89@users.noreply.github.com>
2025-11-08 03:54:14 -0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,10 @@
 // Root build.gradle.kts (Project level)
 
 plugins {
-    // Updated Android Gradle Plugin (AGP) to a recommended stable version
+    // Defining plugin versions for the entire project
     id("com.android.application") version "8.6.1" apply false
     id("com.android.library") version "8.6.1" apply false
     
-    // Kotlin Gradle Plugin (KGP) - using the latest stable 2.2.21
     id("org.jetbrains.kotlin.android") version "2.2.21" apply false 
-
-    // Compose Compiler Plugin using the new KGP 2.0+ approach
     id("org.jetbrains.kotlin.plugin.compose") version "2.2.21" apply false 
 }
